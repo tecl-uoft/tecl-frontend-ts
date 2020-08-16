09f99dbbf063e5324a7f242dd67ef2df09236674
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-08-16 15:03:41 -0400

    added error and loading pages

---
+++ src/pages/ErrorNotFound/ErrorNotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ErrorNotFound() {
+  return (
+    <div className="h-screen w-full flex justify-center">
+      <h3 className="rounded-lg text-3xl px-12 flex flex-col justify-center">
+        Error 404: Page Not Found
+      </h3>
+    </div>
+  );
+}
+
+export default ErrorNotFound;
---
+++ src/pages/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="h-screen w-full flex justify-center">
+      <h3 className=" rounded-lg text-3xl px-12 flex flex-col justify-center">
+        Loading...
+      </h3>
+    </div>
+  );
+}
+
+export default Loading;
