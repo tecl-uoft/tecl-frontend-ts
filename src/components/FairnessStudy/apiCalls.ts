c7580c0027f2567a21f7cdc11c8b0a547a058473
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-08-17 18:11:04 -0400

    added api calls file

---
+++ src/components/FairnessStudy/apiCalls.ts
@@ -0,0 +1,38 @@
+/* Get randomized numbers needed for study setup */
+export function getSetup(
+  setResult: (result: any) => void,
+  setError: (err: any) => void,
+  setIsLoaded: (isLoaded: boolean) => void
+) {
+  fetch("/api/v1/fairness-study/setup", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      setIsLoaded(true);
+      setResult(result);
+    })
+    .catch((err) => setError(err));
+}
+
+/* Send the results of the study */
+export function postResults(
+  body: any,
+  setResult: (result: any) => void,
+  setError: (err: any) => void,
+  setIsSent: (sentStatus: boolean) => void
+) {
+  fetch("/api/v1/fairness-study/results", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      setIsSent(true);
+      setResult(res);
+    })
+    .catch((err) => setError(err));
+}
