9f98b246424afff6500440e753faf7b5ce146622
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-08-17 17:52:35 -0400

    inital fairness study component

---
+++ src/components/FairnessStudy/FairnessStudy.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function FairnessStudy() {
+    const [randomizedElements, setRandomizedElements] = useState(null);
+    const [studyState, setStudyState] = useState("askConsent")
+    
+    return (
+        <div id="fairness-study">
+           
+        </div>
+    )
+}
+
+
+export default FairnessStudy
