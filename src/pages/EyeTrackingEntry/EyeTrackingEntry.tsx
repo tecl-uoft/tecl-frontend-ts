c487be56f707f0d52f86604d8073bd6c6775b3ba
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-09-09 16:32:35 -0400

    updated study name

--- src/pages/EyeTrackingEntry/EyeTrackingEntry.tsx
+++ src/pages/EyeTrackingEntry/EyeTrackingEntry.tsx
@@ -5,7 +5,7 @@ function EyeTrackingEntry() {
   return (
     <div id="eye-tracking-entry">
       <StudyEntryBlock
-        studyTitle="Frogger"
+        studyTitle="Eye Tracking"
         ageRange="0 to 2 years old"
         studyDesc="This study tracks the eyes and which area of the screen they are looking at."
         studyWorkDesc="This study tracks the eyes and which area of the screen they are looking at.
