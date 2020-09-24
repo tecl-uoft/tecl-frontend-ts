33dd378125273843d35fc017d9e6449d2589ca74
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-09-24 07:55:29 -0400

    added remove click

--- src/components/MeetingCalendar/MeetingCalendar.tsx
+++ src/components/MeetingCalendar/MeetingCalendar.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import "./meetingCalendar.css";
-import FullCalendar, { DateSelectArg } from "@fullcalendar/react";
+import FullCalendar, { DateSelectArg, EventClickArg } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 function MeetingCalendar() {
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    const title = prompt("Event Title:");
+    const calendarApi = selectInfo.view.calendar;
+    if (title) {
+      calendarApi.addEvent({
+        title: title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+    calendarApi.unselect();
+  }
+
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    if (prompt(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove()
+    }
+  }
+
   return (
     <div className="pb-6">
       <FullCalendar
@@ -17,19 +37,8 @@ function MeetingCalendar() {
         selectable
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="timeGridWeek"
-        select={(selectInfo: DateSelectArg) => {
-          const title = prompt("Event Title:");
-          const calendarApi = selectInfo.view.calendar;
-          if (title) {
-            calendarApi.addEvent({
-              title: title,
-              start: selectInfo.startStr,
-              end: selectInfo.endStr,
-              allDay: selectInfo.allDay,
-            });
-          }
-          calendarApi.unselect();
-        }}
+        select={handleDateSelect}
+        eventClick={handleEventClick}
       />
     </div>
   );
