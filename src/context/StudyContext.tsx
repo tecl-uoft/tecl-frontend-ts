620bd27a969fab942cd35f3a385d5f98bea6b900
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-09-30 18:36:48 -0400

    added study state

--- src/context/StudyContext.tsx
+++ src/context/StudyContext.tsx
@@ -1,14 +1,30 @@
-import React, { createContext } from 'react'
-import { Props } from './commonTypes'
-
+import React, { createContext, useContext, useState } from "react";
+import StudyService from "../services/StudyService";
+import { Props } from "./commonTypes";
 
 export const StudyContext = createContext<any | undefined>(undefined);
 
 export function StudyProvider({ children }: Props) {
+  const [studyState, setStudyState] = useState<any>(undefined);
 
+  const createStudy = (study: any) => {
+    StudyService.create(study).then((studyRes) => {
+      setStudyState(studyRes);
+    }).catch((err) => {
+        alert(`Error ${err.code}: ${err.message}`)
+    });
+  };
 
-    return (
-        <StudyContext.Provider value={{}}>{children}</StudyContext.Provider>
-    )
+  const defaultContextValue = {
+    createStudy,
+    studyState
+  };
+
+  return (
+    <StudyContext.Provider value={defaultContextValue}>
+      {children}
+    </StudyContext.Provider>
+  );
 }
 
+export const useStudy = () => useContext(StudyContext);
