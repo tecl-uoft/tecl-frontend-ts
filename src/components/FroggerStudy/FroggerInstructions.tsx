0d41ca5803e31ecf5fd62caf8a9c87a5fd8a7a58
Arnav Verma <arnav.verma@mail.utoronto.ca>
2020-11-24 04:26:54 -0500

    updated frogger padding

--- src/components/FroggerStudy/FroggerInstructions.tsx
+++ src/components/FroggerStudy/FroggerInstructions.tsx
@@ -8,12 +8,12 @@ interface IFroggerInstructionsProps {
 function FroggerInstructions(props: IFroggerInstructionsProps) {
   const { nextState } = props;
   return (
-    <div className="container mt-4 mx-auto px-2 pt-4 mb-16">
+    <div className="container px-2 pt-4 mx-auto mt-4 mb-16">
       <StudyTitleText text={"Watch the instructional video below."} />
-      <h4 className="text-2xl text-center text-gray-800 mb-4 mt-4">
+      <h4 className="mt-4 mb-4 text-2xl text-center text-gray-800">
         Make sure to learn the objective of the game.
       </h4>
-      <video className="px-6 focus:outline-none" controls>
+      <video className="px-32 focus:outline-none" controls>
         <source
           src="https://tecl-online-assets.s3.ca-central-1.amazonaws.com/Frogger_Instructions_v6.mp4"
           type="video/mp4"
@@ -23,7 +23,7 @@ function FroggerInstructions(props: IFroggerInstructionsProps) {
       <div className="flex justify-around mt-6">
         <button
           onClick={() => nextState()}
-          className="bg-orange-200 hover:bg-orange-400 font-bold w-full rounded-lg py-4 px-8 shadow-lg focus:outline-none uppercase tracking-wider"
+          className="w-full px-8 py-4 font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400 focus:outline-none"
         >
           Next
         </button>
