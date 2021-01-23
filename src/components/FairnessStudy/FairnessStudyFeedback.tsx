3e2200e567e0e83623e1ad36d9325668290f7a41
rgelpi <rebekah.gelpi@gmail.com>
2021-01-23 15:22:14 -0500

    change prolific completion code

--- src/components/FairnessStudy/FairnessStudyFeedback.tsx
+++ src/components/FairnessStudy/FairnessStudyFeedback.tsx
@@ -11,7 +11,7 @@ function FairnessStudyFeedback() {
   useEffect(() => {
     window.setTimeout(() => {
       window.open(
-        "https://app.prolific.co/submissions/complete?cc=5BA4CA97",
+        "https://app.prolific.co/submissions/complete?cc=1A0BAD35",
         "_blank"
       );
     }, 1000);
@@ -34,7 +34,7 @@ function FairnessStudyFeedback() {
         If it does not, please click{" "}
         <a
           className="text-orange-500"
-          href="https://app.prolific.co/submissions/complete?cc=5BA4CA97"
+          href="https://app.prolific.co/submissions/complete?cc=1A0BAD35"
           rel="noopener noreferrer"
           target="_blank"
         >
