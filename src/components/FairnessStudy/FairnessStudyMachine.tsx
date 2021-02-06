b8741733674ca646ab6471866b36a934179246bf
rgelpi <rebekah.gelpi@gmail.com>
2021-02-06 00:36:44 -0500

    add throwAgain function at end of study

--- src/components/FairnessStudy/FairnessStudyMachine.tsx
+++ src/components/FairnessStudy/FairnessStudyMachine.tsx
@@ -14,6 +14,8 @@ function FairnessStudyMachine(props: IFairnessStudyMachineProps) {
   const choice = (alienUser.name === alienA.name) ? 0 : 
     (alienUser.name === alienB.name) ? 1 : 2;
   const [pickedAlien, setPickedAlien] = useState("");
+  const [hasPickedAlien, setHasPickedAlien] = useState(false);
+  const [throwAgain, setThrowAgain] = useState("");
 
   return ( 
     <div className="container flex flex-col mx-auto my-32">
@@ -34,7 +36,9 @@ function FairnessStudyMachine(props: IFairnessStudyMachineProps) {
         </button>
         <button style={{maxWidth: "50%"}}
           onClick={() => {
-            setTrialFunc();
+            // setTrialFunc();
+            setHasPickedAlien(true);
+            setShowPlayText(true);
           }}
             className={`bg-red-100 hover:bg-red-300 w-full mt-6 font-bold rounded-lg py-4 px-8 shadow-lg focus:outline-none uppercase tracking-wider`}
           >
@@ -42,28 +46,53 @@ function FairnessStudyMachine(props: IFairnessStudyMachineProps) {
         </button>
       </div>
       </div> )
-         : ( 
+         : !hasPickedAlien ? ( 
         <div className="flex justify-around text-gray-800 text-2xl mb-16">
             <AlienDisplay
                 Alien={(choice === 0) ? alienB : alienA }
                 pickedAlien={pickedAlien}
                 setPickedAlien={setPickedAlien}
-                setTrialFunc={setTrialFunc}
+                setHasPickedAlien={setHasPickedAlien}
+                // setTrialFunc={setTrialFunc}
               />
             <AlienDisplay        
                 Alien={(choice === 2) ? alienB : alienC }
                 pickedAlien={pickedAlien}
                 setPickedAlien={setPickedAlien}
-                setTrialFunc={setTrialFunc}
+                setHasPickedAlien={setHasPickedAlien}
+                // setTrialFunc={setTrialFunc}
               />
-      </div> ) 
+      </div> ) :
+      (
+        <div>
+          <div className="text-2xl font-bold text-gray-800 mr-0 mb-16 flex justify-around">
+          <p>
+            Now it's your turn to throw the ball one more time!
+          </p>
+          </div>
+        <div className="flex justify-around text-gray-800 text-2xl mb-16">
+        <AlienDisplay2
+            Alien={ alienA }
+            throwAgain={throwAgain}
+            setThrowAgain={setThrowAgain}
+            setTrialFunc={setTrialFunc}
+          />
+        <AlienDisplay2       
+            Alien={ alienB }
+            throwAgain={throwAgain}
+            setThrowAgain={setThrowAgain}
+            setTrialFunc={setTrialFunc}
+          />
+  </div>
+  </div>
+      )
       } </>
     </div>
   );
 }
 
 function AlienDisplay(props: any) {
-  const { Alien, pickedAlien, setPickedAlien, setTrialFunc } = props;
+  const { Alien, pickedAlien, setPickedAlien, setHasPickedAlien } = props;
 
   return (
     <div
@@ -76,6 +105,33 @@ function AlienDisplay(props: any) {
             },
             body: JSON.stringify({
               pickedAlien: pickedAlien,
+              }),
+            });        
+        setHasPickedAlien(true);
+        
+      }}
+      className={`mt-0 mx-32 hover:text-orange-500 cursor-pointer`}
+    >
+      {<Alien.Front />}
+      <p className={`z-40 alien-label font-bold`}>{Alien.name} </p>
+    </div>
+  );
+}
+
+function AlienDisplay2(props: any) {
+  const { Alien, throwAgain, setThrowAgain, setTrialFunc } = props;
+
+  return (
+    <div
+      onClick={() => { 
+        setThrowAgain(Alien.name);
+        fetch("/api/v1/fairness-study/results-kids", {
+          method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              throwAgain: throwAgain,
               }),
             });        
         setTrialFunc();
