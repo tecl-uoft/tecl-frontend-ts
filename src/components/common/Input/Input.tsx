c7341a22022d7e4f9e36c98a1b68f6d0b5e65317
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-03-03 22:12:15 -0500

    added date change fix

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,7 +1,6 @@
 import { DateTime } from "luxon";
 import React, { Dispatch, SetStateAction } from "react";
 import DatePicker from "react-date-picker";
-import "./DatePicker.css";
 import "./input.css";
 
 function Input(props: {
@@ -14,20 +13,34 @@ function Input(props: {
   max?: string | number;
 }) {
   if (props.type === "date") {
+    const datePickChange = (e: Date | Date[]) => {
+      if (e === null) {
+        props.valueSetter(DateTime.local().toFormat("yyyy-MM-dd"));
+        return;
+      }
+      const givenYearTime = e.toLocaleString().split("/")[2];
+      const givenYearNum = parseInt(givenYearTime.split(",")[0]);
+      if (givenYearNum > 3000 || givenYearNum < 1900) {
+        return;
+      }
+
+      const formattedDate = DateTime.fromJSDate(
+        new Date(e.toString())
+      ).toFormat("yyyy-MM-dd");
+
+      props.valueSetter(formattedDate);
+    };
+
     return (
       <DatePicker
         className={
           "w-full block p-2 mb-2 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none select-none focus:outline-none focus:bg-white focus:border-gray-500"
         }
-        clearIcon={null}
+        calendarClassName={"rounded-lg"}
         value={
           props.value ? DateTime.fromISO(props.value).toJSDate() : new Date()
         }
-        onChange={(e) =>
-          props.valueSetter(
-            DateTime.fromJSDate(new Date(e.toString())).toFormat("yyyy-MM-dd")
-          )
-        }
+        onChange={datePickChange}
       />
     );
   }
