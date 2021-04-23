01b4d441967d542c15a0a0d9fac6d115c89df387
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-04-23 02:51:06 -0400

    added creaive questionaire

--- src/components/FroggerStudy/DemographicQuestions.tsx
+++ src/components/FroggerStudy/DemographicQuestions.tsx
@@ -5,16 +5,20 @@ import * as questionAndChocicesDefault from "./demoQ.json";
 
 function DemographicQuestions(props: { nextState: () => void }) {
   const { nextState } = props;
-  const [demoState, setDemoState] = useState(2);
+  const [demoState, setDemoState] = useState(0);
 
   const updateState = (demoState: number) => {
     switch (demoState) {
       case 0:
         return <InitalInst setDemoState={setDemoState} />;
       case 1:
-        return <Questions nextState={nextState} />;
+        return <Questions nextState={setDemoState} />;
       case 2:
-        return <MCQuestions nextState={nextState} />;
+        return <MCQuestions nextState={setDemoState} />;
+      case 3:
+        return <PrefrenceQs nextState={setDemoState} />;
+      case 4:
+        return <CreativeQs nextState={setDemoState} />;
       default:
         return <> </>;
     }
@@ -22,7 +26,120 @@ function DemographicQuestions(props: { nextState: () => void }) {
   return updateState(demoState);
 }
 
-function MCQuestions(props: { nextState: () => void }) {
+function CreativeQs(props: { nextState: (demoState: number) => void }) {
+  const [response, setResponse] = useState<{
+    [key: number]: {
+      question: string;
+      response: string | number;
+    };
+  }>({});
+
+  const scale = [1, 2, 3, 4, 5];
+  const scaleLabels = [
+    "not true at all",
+    "slightly true",
+    "somewhat true",
+    "moderately true",
+    "always true",
+  ];
+
+  const questions = questionAndChocicesDefault.creative;
+  const setDemoState = () => props.nextState(4);
+  return (
+    <div>
+      <Banner forChild={false} />
+      <h2 className="mt-6 text-4xl font-bold tracking-wide text-center">
+        Creativity Questionnaire
+      </h2>
+      <div className="w-3/4 mx-auto mb-4 space-y-6 text-left md:w-1/2">
+        <p>
+          Below is a list of adjectives. Please indicate how much each adjective
+          describes your child by checking the appropriate space. If you do not
+          know the answer to or feel comfortable answering any questions, please
+          leave the response blank.
+        </p>
+        <h3 className="flex flex-col mx-auto text-xl text-left">
+          <p>Use the following response items:</p>
+          {scale.map((rating, idx) => {
+            return (
+              <p className="text-lg" key={idx}>
+                {rating} = {scaleLabels[idx]}
+              </p>
+            );
+          })}
+        </h3>
+      </div>
+      <div className="w-3/4 h-1 mx-auto bg-blue-200 rounded-lg md:w-1/2" />
+      <div className="flex flex-col justify-center w-3/4 mx-auto space-y-8 md:w-1/2">
+        {questions.map((q, idx) => {
+          return (
+            <div>
+              <LikertScale
+                scale={scale}
+                question={idx + 1 + ". " + q}
+                responseSetter={(res) =>
+                  setResponse((r) => {
+                    r[idx] = { question: q, response: res };
+                    return r;
+                  })
+                }
+              />
+              <div className="w-full h-1 bg-blue-200 rounded-lg" />
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-center w-full my-8">
+        <NextButton setDemoState={setDemoState} />
+      </div>
+    </div>
+  );
+}
+
+function PrefrenceQs(props: { nextState: (demoState: number) => void }) {
+  const [response, setResponse] = useState<{
+    [key: number]: {
+      question: string;
+      response: { select: string; num: number };
+    };
+  }>({});
+
+  const questions = questionAndChocicesDefault.prefrence;
+  const setDemoState = () => props.nextState(4);
+  return (
+    <div>
+      <Banner forChild={false} />
+      <h2 className="mt-6 text-4xl font-bold tracking-wide text-center">
+        Parenting Preferences Questionnaire
+      </h2>
+      <div className="w-3/4 h-1 mx-auto bg-blue-200 rounded-lg md:w-1/2" />
+      <div className="w-3/4 mx-auto md:w-1/2">
+        {questions.map((qa, idx) => {
+          return (
+            <div>
+              <MultiChoice
+                choices={qa.choices}
+                question={idx + 1 + ". " + qa.question}
+                responseSetter={(res) =>
+                  setResponse((r) => {
+                    r[idx] = { question: qa.question, response: res };
+                    return r;
+                  })
+                }
+              />
+              <div className="w-full h-1 bg-blue-200 rounded-lg"> </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-center w-full my-8">
+        <NextButton setDemoState={setDemoState} />
+      </div>
+    </div>
+  );
+}
+
+function MCQuestions(props: { nextState: (demoState: number) => void }) {
   const [response, setResponse] = useState<{
     [key: number]: {
       question: string;
@@ -30,9 +147,34 @@ function MCQuestions(props: { nextState: () => void }) {
     };
   }>({});
   const questionAndChocices = questionAndChocicesDefault.main;
+  const setDemoState = () => props.nextState(3);
+
   return (
     <div>
-      <div>adsjfdsalkfkjdfhasdkfjahsf</div>
+      <Banner forChild={false} />
+      <div className="w-3/4 mx-auto space-y-12 text-lg font-bold text-left">
+        <h2 className="mt-6 text-4xl font-bold tracking-wide text-center">
+          Participant Demographics Survey
+        </h2>
+        <p>
+          {" "}
+          Instructions:
+          <br /> Thank you for taking the time to complete our Participants
+          Demographics Information Survey with the Toronto Early Cognition Lab
+          at the University of Toronto. <br /> <br />
+          In order to ensure that our participants and their families represent
+          a diversity of backgrounds in race, gender, ethnicity, age,
+          socio-economic statuses and more, we are collecting the following
+          information to better direct our recruiting efforts within the lab.{" "}
+          <br /> <br /> Information you provide in this survey will remain
+          anonymous and will be used by lab personnel only. If we use this
+          information for any of our analyses, as with all your data, the
+          information provided here will be anonymized. <br /> <br /> Please
+          complete a survey for each child who has participated in our studies
+          using the Participant ID that has been provided to you. 
+        </p>
+        <div className="w-full h-1 bg-blue-200 rounded-lg" />
+      </div>
       <div className="w-3/4 mx-auto md:w-1/2 ">
         {questionAndChocices.map((qa, idx) => {
           return (
@@ -52,12 +194,22 @@ function MCQuestions(props: { nextState: () => void }) {
           );
         })}
       </div>
+      <div className="flex justify-center w-full my-6 ">
+        <NextButton setDemoState={setDemoState} />
+      </div>
     </div>
   );
 }
 
-function Questions(props: { nextState: () => void }) {
+function Questions(props: { nextState: (demoState: number) => void }) {
   const scale = [1, 2, 3, 4, 5];
+  const scaleLabels = [
+    "never",
+    "a few times per year",
+    "a few times per month",
+    "a few times per week",
+    "daily",
+  ];
   const questions = [
     "How often Do you play video games?",
     "How often do you play platforming games (e.g., Mario)? ",
@@ -66,14 +218,26 @@ function Questions(props: { nextState: () => void }) {
   const [response, setResponse] = useState<{
     [key: number]: { question: string; response: string | number };
   }>({});
-
+  const setDemoState = () => props.nextState(2);
   return (
-    <div>
-      <div className="flex flex-col w-full pb-8 space-y-4 text-2xl">
-        <div>For Child</div>
-        <h2>Experience with Video Games</h2>
-        Use the following response items:
-        <div className="w-3/4 mx-auto md:w-1/2 ">
+    <div className="w-full">
+      <div className="flex flex-col w-full pb-8 mb-6 space-y-4 text-2xl">
+        <Banner forChild={true} />
+        <h2 className="text-2xl font-bold text-center">
+          Experience with Video Games
+        </h2>
+        <h3 className="flex flex-col mx-auto text-xl text-left">
+          <p>Use the following response items:</p>
+          {scale.map((rating, idx) => {
+            return (
+              <p className="text-lg" key={idx}>
+                {rating} = {scaleLabels[idx]}
+              </p>
+            );
+          })}
+        </h3>
+
+        <div className="flex flex-col justify-center w-3/4 mx-auto space-y-8 md:w-1/2">
           {questions.map((q, idx) => {
             return (
               <div>
@@ -92,6 +256,7 @@ function Questions(props: { nextState: () => void }) {
             );
           })}
         </div>
+        <NextButton setDemoState={setDemoState} />
       </div>
     </div>
   );
@@ -107,12 +272,33 @@ function InitalInst(props: { setDemoState: (state: number) => void }) {
         fill out -- <br />
         we’ll let you know on every screen who should fill out the form!
       </p>
-      <button
-        onClick={() => props.setDemoState(1)}
-        className="w-3/4 px-8 py-4 mx-auto text-lg font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400 focus:outline-none"
-      >
-        Next
-      </button>
+      <NextButton setDemoState={() => props.setDemoState(1)} />
+    </div>
+  );
+}
+
+function NextButton({ setDemoState }: { setDemoState: () => void }) {
+  return (
+    <button
+      onClick={setDemoState}
+      className="w-3/4 px-8 py-4 mx-auto text-lg font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400 focus:outline-none"
+    >
+      Next
+    </button>
+  );
+}
+
+function Banner({ forChild }: { forChild: boolean }) {
+  return (
+    <div
+      className={`flex justify-center w-full py-4 text-4xl font-bold tracking-wide ${
+        forChild ? "bg-blue-200" : "bg-green-200"
+      }`}
+    >
+      <div className="my-auto uppercase">
+        {" "}
+        For {"  "} {forChild ? "Child" : "Adult"}
+      </div>
     </div>
   );
 }
