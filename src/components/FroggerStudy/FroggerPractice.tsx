413368fcc0cd336480246d423ccc2470e86bb1f2
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-05-27 18:59:58 -0400

    updated is mod

--- src/components/FroggerStudy/FroggerPractice.tsx
+++ src/components/FroggerStudy/FroggerPractice.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Unity, { UnityContent } from "react-unity-webgl";
 import StudyTitleText from "../common/StudyTitleText";
 
@@ -8,8 +8,9 @@ interface IFroggerPracticeProps {
 
 function FroggerPractice(props: IFroggerPracticeProps) {
   const { nextState } = props;
-  const [gameOver, setGameOver] = useState(false)
-  // only on mod study or no?
+  const [gameOver, setGameOver] = useState(false);
+  const [isMod, setIsMod] = useState(true);
+  // only on mod study ortrue
   const unityContent = new UnityContent(
     "/scripts/Frogger_Practice_5.7.21/Frogger_Practice_5.7.21.json",
     "/scripts/Frogger_Practice_5.7.21/UnityLoader.js"
@@ -17,10 +18,15 @@ function FroggerPractice(props: IFroggerPracticeProps) {
     //"/scripts/frogger_practice/UnityLoader.js"
   );
 
+  useEffect(() => {
+    if (!window.location.pathname.includes("mod")) {
+      setIsMod(false);
+    }
+  }, []);
+
   unityContent.on("GameOver", () => {
-  
     setGameOver(true);
-  })
+  });
 
   return (
     <div className="px-2 pt-4 mx-auto mb-16">
@@ -39,14 +45,16 @@ function FroggerPractice(props: IFroggerPracticeProps) {
       <div className="px-32">
         <Unity unityContent={unityContent} />
       </div>
-      {gameOver && <div className="flex justify-around mt-6">
-        <button
-          onClick={() => nextState()}
-          className="w-full px-8 py-4 font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400 focus:outline-none"
-        >
-          Next
-        </button>
-      </div>}
+      {(gameOver || isMod) && (
+        <div className="flex justify-around mt-6">
+          <button
+            onClick={() => nextState()}
+            className="w-full px-8 py-4 font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400 focus:outline-none"
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 }
