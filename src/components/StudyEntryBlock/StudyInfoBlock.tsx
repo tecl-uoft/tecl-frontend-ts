6f85b4a26d40e2a52b8b5dd4076dcadae765cc64
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-09-07 16:07:17 -0400

    updatedfrogger study entry

--- src/components/StudyEntryBlock/StudyInfoBlock.tsx
+++ src/components/StudyEntryBlock/StudyInfoBlock.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import IStudyEntryBlockProps from "./IStudyEntryBlockProps";
 
-function StudyEntryBlock(props: IStudyEntryBlockProps) {
+interface IStudyInfoBlockProps {
+  /* Title of the study */
+  studyTitle: string;
+
+  /* Age range of the study */
+  ageRange: string;
+
+  /* Long  desription of the study */
+  studyDesc: string;
+
+  /* A description of how the study works */
+  studyWorkDesc: string | React.ReactNode;
+
+  /* A URI of the actual study */
+  studyTrialURI: string;
+
+  /* A URI of the study image preview */
+  studyImgURI: string;
+
+  /* Alternitive text for the image */
+  studyImgAlt: string;
+
+  onClickPic?: () => void;
+}
+
+function StudyEntryBlock(props: IStudyInfoBlockProps) {
   return (
     <div className="container px-2 pt-4 mx-auto">
       <h2 className="text-4xl font-bold text-center text-gray-800">
@@ -12,7 +36,7 @@ function StudyEntryBlock(props: IStudyEntryBlockProps) {
         Information
       </h3>
       <div className="flex flex-wrap mb-16 text-gray-800 fair-study">
-        <div className="w-full  md:w-1/2">
+        <div className="w-full md:w-1/2">
           <p className="mb-8 text-xl text-gray-600">
             <b>Participant Ages: {props.ageRange} </b> <br />
             {props.studyDesc}
@@ -20,8 +44,8 @@ function StudyEntryBlock(props: IStudyEntryBlockProps) {
           <h4 className="mb-3 text-3xl font-bold"> How does it work? </h4>
           <p className="mb-8 text-xl text-gray-600">{props.studyWorkDesc}</p>
         </div>
-        <div className="w-full px-2 text-gray-800 bg-white rounded-lg md:w-1/2 hover:bg-gray-100 focus:outline-none hover:text-orange-500">
-          <Link to={props.studyTrialURI}>
+        <div onClick={props.onClickPic} className="w-full px-2 text-gray-800 bg-white rounded-lg md:w-1/2 hover:bg-gray-100 focus:outline-none hover:text-orange-500">
+          <Link className="" to={props.studyTrialURI}>
             <img
               className="px-6 pt-2"
               src={process.env.PUBLIC_URL + props.studyImgURI}
--- src/pages/FroggerStudyEntry/FroggerStudyEntry.tsx
+++ src/pages/FroggerStudyEntry/FroggerStudyEntry.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import toast from "react-hot-toast";
+import { notify } from "../../components/Notification";
 import StudyEntryBlock from "../../components/StudyEntryBlock";
 
 function FroggerStudyEntry() {
@@ -9,7 +11,7 @@ function FroggerStudyEntry() {
         ageRange="7 & 8 years old or 18 to 55 years old"
         studyDesc="We are interested in how social context changes attempts to solve the game."
         studyWorkDesc={
-          <div className="px-4 text-md" >
+          <div className="px-4 text-md">
             You’ll provide us your email, and we’ll send you a link to play as
             Frog and solve a simple platforming game. Then, you will answer some
             questions about your experience and fill out some surveys. It is
@@ -26,6 +28,19 @@ function FroggerStudyEntry() {
               appropriate study.
             </span>
           </div>
+        }
+        onClickPic={() =>
+          toast(
+            "Please note that video and audio recording are required for this game so that we can perform quality control. You will not be compensated if you do not provide audio and video. Please contact tecl.psychology@utoronto.ca if you are uncomfortable consenting to audio and video recording, and we will be happy to find a more appropriate study.",
+            {
+              icon: "⚠️",
+              style: {
+                border: "1px solid #713200",
+                padding: "16px",
+                color: "#713200",
+              },
+            }
+          )
         }
         studyImgURI="/assets/frogger_study.svg"
         studyImgAlt="Frogger Study"
