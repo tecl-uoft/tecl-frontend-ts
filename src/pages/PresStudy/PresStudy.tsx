9c96a14ece52c2012d45bab35183ec590afbe823
Arnav Verma <arnav.verma@mail.utoronto.ca>
2022-05-24 13:01:49 -0400

    finished all cases for first stoy

--- src/pages/PresStudy/PresStudy.tsx
+++ src/pages/PresStudy/PresStudy.tsx
@@ -1,7 +1,10 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import image_map from "./image_map.json";
+import image_map_sb1 from "./image_map_sb1.json"
+import image_map_sb2 from "./image_map_sb2.json"
 import ysFixWebmDuration from "fix-webm-duration";
 import { notify } from "../../components/Notification";
+import { useParams } from "react-router";
 
 interface CanvasElement extends HTMLCanvasElement {
   captureStream(frameRate?: number): MediaStream;
@@ -17,7 +20,21 @@ function PresStudy(props: React.FC<{}>) {
   }>();
 
   useEffect(() => {
-    setImageMap(image_map.story_links);
+    const params = new URLSearchParams(window.location.search);
+    const story = params.get("story");
+    switch (story) {
+      case "ex":
+        setImageMap(image_map.story_links);
+        break;
+      case "sb1":
+        setImageMap(image_map_sb1.story_links);
+        break;
+      case "sb2":
+        setImageMap(image_map_sb2.story_links);
+        break;
+      default:
+        setImageMap(image_map.story_links);
+    }
   }, []);
 
   const onNextClick = useCallback(() => {
@@ -57,11 +74,103 @@ function PresStudy(props: React.FC<{}>) {
     }
   }, [videoRef, setVideoRecorder]);
 
+
+
+  function showButtons(page: number) {
+    const choiceComponent = (choices: { name: string, onClick: () => void }[]) => {
+      return (
+        <div className="flex justify-around px-2 my-6 space-x-2">
+          {
+            choices.map((choice) => {
+              return <button
+                onClick={choice.onClick}
+                className="w-full px-8 py-4 font-bold tracking-wider uppercase bg-green-200 rounded-lg shadow-lg hover:bg-green-400"
+              >
+                {choice.name}
+              </button>
+            })
+          }
+        </div>
+      )
+    }
+
+    switch (page) {
+      case 8:
+        return choiceComponent([
+          { name: "Blake", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Avery", onClick: () => setCurrImage(i => i + 1) }])
+
+      case 11:
+        return choiceComponent([
+          { name: "Band-Aid", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Toys", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 18:
+        return choiceComponent([
+          { name: "Blake", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Avery", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 21:
+        return choiceComponent([
+          { name: "Soccer Ball", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Puzzle Box", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 28:
+        return choiceComponent([
+          { name: "Blake", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Avery", onClick: () => setCurrImage(i => i + 1) }])
+
+      case 31:
+        return choiceComponent([
+          { name: "Milk", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Sandwhich", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 36:
+        return choiceComponent([
+          { name: "Avery", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Blake", onClick: () => setCurrImage(i => i + 1) },])
+      
+      case 39:
+        return choiceComponent([
+          { name: "Sweater", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Stickers", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+      case 45:
+        return choiceComponent([
+          { name: "Blake", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Connor", onClick: () => setCurrImage(i => i + 2) },])
+
+      case 48:
+        return choiceComponent([
+          { name: "Hat", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Toy", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case image_map.story_links.length:
+        return choiceComponent([
+          { name: "Finish", onClick: onFinishClick }])
+
+      default:
+        return <div className="flex justify-around px-2 my-6 space-x-2">
+          <button
+            onClick={onNextClick}
+            className="w-1/2 px-8 py-4 font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400"
+          >
+            Next
+          </button>
+        </div>
+    }
+
+  }
+
   return (
     <div className="h-screen bg-blue-200 ">
       <div className="flex justify-center w-screen py-1">
         <img
-          className="w-2/4 pr-4"
+          className="pr-4 "
           alt={`map sequence ${currImage}`}
           src={imageMap[currImage]}
         />
@@ -78,35 +187,7 @@ function PresStudy(props: React.FC<{}>) {
           </video>
         </div>
       </div>
-      {currImage !== image_map.story_links.length - 1 ? (
-        <div className="flex justify-around px-2 my-6 space-x-2">
-          {currImage === 0 ? (
-            <div className="w-1/2 px-8 py-4" />
-          ) : (
-            <button
-              onClick={onBackClick}
-              className="w-1/2 px-8 py-4 font-bold tracking-wider uppercase bg-red-200 rounded-lg shadow-lg hover:bg-red-400"
-            >
-              Back
-            </button>
-          )}
-          <button
-            onClick={onNextClick}
-            className="w-1/2 px-8 py-4 font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400"
-          >
-            Next
-          </button>
-        </div>
-      ) : (
-        <div className="flex justify-around px-2 my-6 space-x-2">
-          <button
-            onClick={onFinishClick}
-            className="w-full px-8 py-4 font-bold tracking-wider uppercase bg-green-200 rounded-lg shadow-lg hover:bg-green-400"
-          >
-            Finish
-          </button>
-        </div>
-      )}
+      {showButtons(currImage + 1)}
     </div>
   );
 }
