a838b4096156528e40bba5fbd55f24b271ffcff4
Arnav Verma <arnav.verma@mail.utoronto.ca>
2022-05-24 13:30:37 -0400

    added all stories

--- src/pages/PresStudy/PresStudy.tsx
+++ src/pages/PresStudy/PresStudy.tsx
@@ -13,15 +13,25 @@ interface CanvasElement extends HTMLCanvasElement {
 function PresStudy(props: React.FC<{}>) {
   const [imageMap, setImageMap] = useState<string[]>([]);
   const [currImage, setCurrImage] = useState(0);
+  const [story, setStory] = useState<"ex" | "sb1" | "sb2">("ex")
   const [videoRecorder, setVideoRecorder] = useState<{
     mediaRecorder: MediaRecorder;
     recordedChunks: Blob[];
     startTime: number;
   }>();
 
+
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const story = params.get("story");
+    if (story === "ex" || story === "sb1" || story === "sb2") {
+      setStory(story)
+    } else {
+      setStory("ex")
+    }
+  }, [])
+
+  useEffect(() => {
     switch (story) {
       case "ex":
         setImageMap(image_map.story_links);
@@ -35,7 +45,7 @@ function PresStudy(props: React.FC<{}>) {
       default:
         setImageMap(image_map.story_links);
     }
-  }, []);
+  }, [story]);
 
   const onNextClick = useCallback(() => {
     setCurrImage((i) => {
@@ -74,9 +84,96 @@ function PresStudy(props: React.FC<{}>) {
     }
   }, [videoRef, setVideoRecorder]);
 
+  function showButtonSb2(page: number) {
+    const choiceComponent = (choices: { name: string, onClick: () => void }[]) => {
+      return (
+        <div className="flex justify-around px-2 my-6 space-x-2">
+          {
+            choices.map((choice) => {
+              return <button
+                onClick={choice.onClick}
+                className="w-full px-8 py-4 font-bold tracking-wider uppercase bg-green-200 rounded-lg shadow-lg hover:bg-green-400"
+              >
+                {choice.name}
+              </button>
+            })
+          }
+        </div>
+      )
+    }
 
+    switch (page) {
+      case 11:
+        return choiceComponent([
+          { name: "Give Croissant", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Keep Croissant", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
 
-  function showButtons(page: number) {
+      case 11:
+        return choiceComponent([
+          { name: "Cam", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Alex", onClick: () => setCurrImage(i => i + 1) }])
+
+      case 19:
+        return choiceComponent([
+          { name: "Give Band-Aid", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Keep Band-Aid", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 28:
+        return choiceComponent([
+          { name: "Felix", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Alex", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 28:
+        return choiceComponent([
+          { name: "Give Blanket", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Keep Blanket", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 31:
+        return choiceComponent([
+          { name: "Alex", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Cam", onClick: () => setCurrImage(i => i + 2) },])
+
+      case 38:
+        return choiceComponent([
+          { name: "Give Extra Balloon", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Keep Extra Balloon", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+      case 41:
+        return choiceComponent([
+          { name: "Alisa", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Alex", onClick: () => setCurrImage(i => i + 1) },])
+
+      case 47:
+        return choiceComponent([
+          { name: "Give the Ladder", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Keep the Ladder", onClick: () => setCurrImage(i => i + 2) },
+          { name: "Other", onClick: () => setCurrImage(i => i + 2) }])
+
+      case 50:
+        return choiceComponent([
+          { name: "Alex", onClick: () => setCurrImage(i => i + 1) },
+          { name: "Alisa", onClick: () => setCurrImage(i => i + 2) },])
+
+      case image_map.story_links.length:
+        return choiceComponent([
+          { name: "Finish", onClick: onFinishClick }])
+
+      default:
+        return <div className="flex justify-around px-2 my-6 space-x-2">
+          <button
+            onClick={onNextClick}
+            className="w-1/2 px-8 py-4 font-bold tracking-wider uppercase bg-orange-200 rounded-lg shadow-lg hover:bg-orange-400"
+          >
+            Next
+          </button>
+        </div>
+    }
+  }
+
+  function showButtonsSb1(page: number) {
     const choiceComponent = (choices: { name: string, onClick: () => void }[]) => {
       return (
         <div className="flex justify-around px-2 my-6 space-x-2">
@@ -132,7 +229,7 @@ function PresStudy(props: React.FC<{}>) {
         return choiceComponent([
           { name: "Avery", onClick: () => setCurrImage(i => i + 2) },
           { name: "Blake", onClick: () => setCurrImage(i => i + 1) },])
-      
+
       case 39:
         return choiceComponent([
           { name: "Sweater", onClick: () => setCurrImage(i => i + 1) },
@@ -187,7 +284,7 @@ function PresStudy(props: React.FC<{}>) {
           </video>
         </div>
       </div>
-      {showButtons(currImage + 1)}
+      {story === "sb1" ? showButtonsSb1(currImage + 1) : showButtonSb2(currImage + 1)}
     </div>
   );
 }
