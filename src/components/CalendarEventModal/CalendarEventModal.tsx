cba2530f5c91dc5fa70ceb57944ec6e2613a14c6
Arnav Verma <arnav.verma@mail.utoronto.ca>
2022-06-16 01:27:50 -0400

    adding appt owner

--- src/components/CalendarEventModal/CalendarEventModal.tsx
+++ src/components/CalendarEventModal/CalendarEventModal.tsx
@@ -36,6 +36,7 @@ function CalendarEventModal(props: ICalendarEventModalProps) {
   const [endRepeat, setEndRepeat] = useState<DateTime | undefined>(undefined);
   const [zoomMeetingID, setZoomMeetingID] = useState("");
   const [parkLocation, setParkLocation] = useState("")
+  const [apptOwner, setApptOwner] = useState("")
 
   /* Sets the shown time interval as study default time interval */
   useEffect(() => {
@@ -327,19 +328,13 @@ function CalendarEventModal(props: ICalendarEventModalProps) {
                     className="block w-full p-2 text-gray-700 bg-gray-200 border rounded cursor-text focus:bg-white"
                   />
                 </div>
-                {/* <div className="flex w-3/4 mx-auto">
+                <div className="w-3/4 mx-auto">
                   <Label text={"Appointment Owner:"} />
-                  <select className="block w-full p-2 text-gray-700 bg-gray-200 border rounded cursor-text focus:bg-white">
+                  <select value={apptOwner} onChange={(e) => {setApptOwner(e.target.value)}} className="block w-full p-2 text-gray-700 bg-gray-200 border rounded cursor-text focus:bg-white">
                     <option value="">{}</option>
                   </select >
-                   <input
-                    value={parkLocation}
-                    onChange={(e) => setParkLocation(e.currentTarget.value)}
-                    placeholder="Elm Street"
-                    type=""
-                    className="block w-full p-2 text-gray-700 bg-gray-200 border rounded cursor-text focus:bg-white"
-                  /> 
-                </div> */}
+                 
+                </div>
 
                 <div
                   className="flex justify-between px-8 mt-2 cursor-pointer"
